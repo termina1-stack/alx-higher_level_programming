6aef5ff10439f46f8f1ef27b8de1b336df518689
Jeff Kirui <kipkoech_g@yahoo.com>
2023-11-02 17:23:50 +0300

    Task 0 to 9

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    from sys import argv, exit
+
+    if len(argv) - 1 != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+
+    operations = {
+        "+": add,
+        "-": sub,
+        "*": mul,
+        "/": div
+    }
+
+    if argv[2] not in list(operations.keys()):
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+
+    # If the validations are Ok, call the function print_format
+    a = int(argv[1])
+    b = int(argv[3])
+    print("{} {} {} = {}".format(a, argv[2], b, operations[argv[2]](a, b)))
